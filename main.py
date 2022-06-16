e1ad0d60a1ea7224413355b93b5b888b4e1dea23
LyQuangToaiCoder <106007459+LyQuangToaiCoder@users.noreply.github.com>
2022-06-16 22:05:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-import telebot
 
-bot = telebot.TeleBot("5534271637:AAG9IdOafmrrOKMGcH5baZRlBZZuw16wHL8")
-
-@bot.message_handler(commands=["start"])
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
+app = ApplicationBuilder().token("5534271637:AAG9IdOafmrrOKMGcH5baZRlBZZuw16wHL8").build()
 
-def send_welcome(message):
+app.add_handler(CommandHandler("hello", hello))
 
-    bot.reply_to(message, "Xin Chào",hello)
-
-@bot.message_handler(commands=["help"])
-
-def send_message(message):
-
-    bot.send_message(message.chat.id, "https://www.youtube.com/channel/UCL8PI42TZ_uaQWVVKUJx9Eg")
-
-
-
-
-bot.polling()
+app.run_polling()
